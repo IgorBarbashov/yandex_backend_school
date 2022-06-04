0f52b1b1901932f4be273710a34a842bc327a379
Igor Barbashov <ibarbashov@freeconferencecall.com>
2022-06-04 21:57:33 +0300

    maze

---
+++ contest/src/maze/Solution.java
@@ -0,0 +1,102 @@
+package maze;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Maze maze = readData();
+        ArrayList<int[]> route = new ArrayList();
+        route.add(maze.S);
+
+        while (route.size() > 0) {
+            int[] P = route.remove(0);
+            int cN = P[0];
+            int cM = P[1];
+
+            maze.field[cN][cM].visited = true;
+
+            if (cN > 0 && maze.field[cN - 1][cM].value != '#' && !maze.field[cN - 1][cM].visited) {
+                maze.field[cN - 1][cM].direction = "U";
+                route.add(new int[]{cN - 1, cM});
+            }
+
+            if (cN < maze.N - 1 && maze.field[cN + 1][cM].value != '#' && !maze.field[cN + 1][cM].visited) {
+                maze.field[cN + 1][cM].direction = "D";
+                route.add(new int[]{cN + 1, cM});
+            }
+
+            if (cM > 0 && maze.field[cN][cM - 1].value != '#' && !maze.field[cN][cM - 1].visited) {
+                maze.field[cN][cM - 1].direction = "L";
+                route.add(new int[]{cN, cM - 1});
+            }
+
+            if (cM < maze.M - 1 && maze.field[cN][cM + 1].value != '#' && !maze.field[cN][cM + 1].visited) {
+                maze.field[cN][cM + 1].direction = "R";
+                route.add(new int[]{cN, cM + 1});
+            }
+        }
+
+        printMaze(maze);
+    }
+
+    static private Maze readData() {
+        try (Scanner in = new Scanner(new File("src/maze/input.txt"))) {
+            String[] measures = in.nextLine().split(" ");
+            int N = Integer.parseInt(measures[0]);
+            int M = Integer.parseInt(measures[1]);
+            Maze maze = new Maze(N, M);
+
+            for (int i = 0; i < N; i++) {
+                String line = in.nextLine();
+                maze.field[i] = new Cell[M];
+                for (int j = 0; j < M; j++) {
+                    maze.field[i][j] = new Cell(line.charAt(j));
+                    if (line.charAt(j) == 'S') {
+                        maze.S[0] = i;
+                        maze.S[1] = j;
+                    }
+                }
+            }
+
+            return maze;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static private void printMaze(Maze maze) {
+        for (Cell[] ints : maze.field) {
+            for (Cell anInt : ints) {
+                System.out.print(anInt.value == '.' ? anInt.direction : anInt.value);
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Cell {
+    char value;
+    boolean visited;
+    String direction;
+
+    public Cell(char value) {
+        this.value = value;
+    }
+}
+
+class Maze {
+    int N;
+    int M;
+    Cell[][] field;
+    int[] S;
+
+    public Maze(int N, int M) {
+        this.N = N;
+        this.M = M;
+        this.field = new Cell[N][M];
+        this.S = new int[2];
+    }
+}
