d6675aa22fe3cfd6f9664b0a3915094c8b83800d
Igor Barbashov <ibarbashov@freeconferencecall.com>
2022-06-05 13:30:41 +0300

    fix product_report solution

--- contest/src/product_report/Solution.java
+++ contest/src/product_report/Solution.java
@@ -5,82 +5,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
-
-public class Solution {
-    public static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-
-    public static void main(String[] args) throws java.text.ParseException {
-        Filter filter = new Filter();
-        JSONArray products = readData(filter);
-        ArrayList<JSONObject> resultList = new ArrayList<>();
-
-        for (Object product : products) {
-            if (validateProductByFilter((JSONObject) product, filter)) {
-                resultList.add((JSONObject) product);
-            }
-        }
-
-        Collections.sort(resultList, new Comparator<JSONObject>() {
-            @Override
-            public int compare(JSONObject o1, JSONObject o2) {
-                return ((Long) o1.get("id")).intValue() - ((Long) o2.get("id")).intValue();
-            }
-        });
-
-        System.out.println(resultList);
-    }
-
-    private static JSONArray readData(Filter filter) {
-        try (Scanner in = new Scanner(new File("src/product_report/input.txt"))) {
-            JSONParser parser = new JSONParser();
-            String jsonString = "{\"products\":" + in.nextLine() + "}";
-            JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
-
-            for (int i = 0; i < 5; i++) {
-                String[] cF = in.nextLine().split(" ");
-                String fN = cF[0];
-                String fV = cF[1];
-
-                if (fN.equals(Filter.FILTER_FIELD_NAME.NAME_CONTAINS.name())) {
-                    filter.setNameContainsFilter(fV);
-                } else if (fN.equals(Filter.FILTER_FIELD_NAME.PRICE_GREATER_THAN.name())) {
-                    filter.setPriceGreaterThanFilter(Integer.parseInt(fV));
-                } else if (fN.equals(Filter.FILTER_FIELD_NAME.PRICE_LESS_THAN.name())) {
-                    filter.setPriceLessThanFilter(Integer.parseInt(fV));
-                } else if (fN.equals(Filter.FILTER_FIELD_NAME.DATE_BEFORE.name())) {
-                    filter.setDateBeforeFilter(fV);
-                } else if (fN.equals(Filter.FILTER_FIELD_NAME.DATE_AFTER.name())) {
-                    filter.setDateAfterFilter(fV);
-                }
-            }
-
-            return (JSONArray) jsonObject.get("products");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        } catch (java.text.ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static boolean validateProductByFilter(JSONObject product, Filter filter) throws java.text.ParseException {
-        String name = (String) product.get("name");
-        int price = ((Long) product.get("price")).intValue();
-        String dateString = (String) product.get("date");
-        Date date = Solution.sdf.parse(dateString);
-
-        if (!name.contains(filter.getNameContainsFilter()) || price > filter.getPriceLessThanFilter() || price < filter.getPriceGreaterThanFilter() || date.before(filter.getDateAfterFilter()) || date.after(filter.getDateBeforeFilter())) {
-            return false;
-        }
-
-        return true;
-    }
-}
 
 class Filter {
     enum FILTER_FIELD_NAME {
@@ -114,7 +41,7 @@ class Filter {
     Date dateAfterFilter;
 
     public void setNameContainsFilter(String nameContainsFilter) {
-        this.nameContainsFilter = nameContainsFilter;
+        this.nameContainsFilter = nameContainsFilter.toLowerCase();
     }
 
     public void setPriceGreaterThanFilter(int priceGreaterThanFilter) {
@@ -131,5 +58,85 @@ class Filter {
 
     public void setDateAfterFilter(String dateAfterFilter) throws java.text.ParseException {
         this.dateAfterFilter = Solution.sdf.parse(dateAfterFilter);
+    }
+}
+
+public class Solution {
+    public static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+    public static void main(String[] args) throws java.text.ParseException {
+        Filter filter = new Filter();
+        JSONArray products = readData(filter);
+        ArrayList<JSONObject> resultList = new ArrayList<>();
+
+        for (Object product : products) {
+            if (validateProductByFilter((JSONObject) product, filter)) {
+                resultList.add((JSONObject) product);
+            }
+        }
+
+        Collections.sort(resultList, new Comparator<JSONObject>() {
+            @Override
+            public int compare(JSONObject o1, JSONObject o2) {
+                return ((Long) o1.get("id")).intValue() - ((Long) o2.get("id")).intValue();
+            }
+        });
+
+        System.out.println(resultList);
+    }
+
+    private static JSONArray readData(Filter filter) {
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(System.in))) {
+            JSONParser parser = new JSONParser();
+            String jsonString = "{\"products\":" + r.readLine() + "}";
+            JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
+
+            for (int i = 0; i < 5; i++) {
+                String[] cF = r.readLine().split(" ");
+                String fN = cF[0];
+                String fV = cF[1];
+
+                if (fN.equals(Filter.FILTER_FIELD_NAME.NAME_CONTAINS.name())) {
+                    filter.setNameContainsFilter(fV);
+                } else if (fN.equals(Filter.FILTER_FIELD_NAME.PRICE_GREATER_THAN.name())) {
+                    filter.setPriceGreaterThanFilter(Integer.parseInt(fV));
+                } else if (fN.equals(Filter.FILTER_FIELD_NAME.PRICE_LESS_THAN.name())) {
+                    filter.setPriceLessThanFilter(Integer.parseInt(fV));
+                } else if (fN.equals(Filter.FILTER_FIELD_NAME.DATE_BEFORE.name())) {
+                    filter.setDateBeforeFilter(fV);
+                } else if (fN.equals(Filter.FILTER_FIELD_NAME.DATE_AFTER.name())) {
+                    filter.setDateAfterFilter(fV);
+                }
+            }
+
+            return (JSONArray) jsonObject.get("products");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (java.text.ParseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean validateProductByFilter(JSONObject product, Filter filter) throws java.text.ParseException {
+        String name = (String) product.get("name");
+        int price = ((Long) product.get("price")).intValue();
+        String dateString = (String) product.get("date");
+        Date date = Solution.sdf.parse(dateString);
+
+        if (
+                !name.toLowerCase().contains(filter.getNameContainsFilter()) ||
+                price > filter.getPriceLessThanFilter() ||
+                price < filter.getPriceGreaterThanFilter() ||
+                date.before(filter.getDateAfterFilter()) ||
+                date.after(filter.getDateBeforeFilter())
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
