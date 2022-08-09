a85a6bebf10a8c61096e779b8e8bd583a165456d
Igor Barbashov <ibarbashov@freeconferencecall.com>
2022-08-09 16:44:55 +0300

    trainee day - short variant

--- trainee_day/solution.js
+++ trainee_day/solution.js
@@ -4,23 +4,15 @@ const data = [
     ["Hillary", "Everest", "South Pole"]  
 ];
 
-const researchers = explorers => {
-    const toponymsObj = explorers
-        .reduce((acc, [researcher, ...toponyms]) => {
-            toponyms.forEach(toponym => {
-                if (!acc[toponym]) {
-                    acc[toponym] = [];
-                }
-                acc[toponym].push(researcher);
-            });
-            return acc;
-        }, {});
+const researchers = explorers => Object.values(explorers
+    .reduce((acc, [researcher, ...toponyms]) => {
+        toponyms.forEach(toponym => {
+            if (!acc[toponym]) {
+                acc[toponym] = [toponym];
+            }
+            acc[toponym].push(researcher);
+        });
+        return acc;
+    }, {}));
     
-    return Object.entries(toponymsObj)
-        .reduce((acc, [researcher, toponyms]) => {
-            acc.push([researcher, ...toponyms]);
-            return acc;
-        }, []);
-};
-
 console.log(researchers(data));
